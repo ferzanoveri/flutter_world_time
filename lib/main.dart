6a03b5b3df0b6a87a191ffa399e2cd27e7609695
ferzanoveri <ferzanoveri@gmail.com>
2025-01-12 16:27:06 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_project_3/pages/choose_location.dart';
+import 'package:flutter_project_3/pages/home.dart';
+import 'package:flutter_project_3/pages/loading.dart';
+
+void main() {
+  runApp(MaterialApp(
+    initialRoute: '/home',
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation(),
+    },
+  ));
+}
---
+++ lib/pages/choose_location.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class ChooseLocation extends StatefulWidget {
+  const ChooseLocation({super.key});
+
+  @override
+  State<ChooseLocation> createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+
+  void getData() async {
+
+  //   simulate network request for a username
+    String username = await Future.delayed(Duration(seconds: 3), () {
+      return 'yoshi';
+    });
+
+    //   simulate network request to get bio of a username
+    String bio = await Future.delayed(Duration(seconds: 2), () {
+      return 'vegan, musician & egg collector';
+    });
+
+    print('$username - $bio');
+
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+    print('hey there!');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.white),
+        backgroundColor: Colors.blue[900],
+        title: Text(
+          'Choose a Location',
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        elevation: 0,
+      ),
+    );
+  }
+}
