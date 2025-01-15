60ccfb393af154d5a9e410a33402504d33a5f5e6
ferzanoveri <ferzanoveri@gmail.com>
2025-01-15 20:57:44 +0700

    passing route data

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -10,14 +10,13 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  String time = 'loading';
-
   void setupWorldTime() async {
-    WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe%2FLondon');
+    WorldTime instance = WorldTime(location: 'London', flag: 'england.png', url: 'Europe%2FLondon');
     await instance.getTime();
-    print(instance.time);
-    setState(() {
-      time = instance.time;
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'time': instance.time,
+      'flag': instance.flag,
     });
   }
 
@@ -32,7 +31,7 @@ class _LoadingState extends State<Loading> {
     return Scaffold(
       body: Padding(
         padding: EdgeInsets.all(50.0),
-        child: Text(time),
+        child: Text('loading'),
       ),
     );
   }
